d91c39de7d928d07e2f4076a7dd2505275296626
Moussa <salisoumoussa1@gmail.com>
2025-11-21 09:08:05 +0100

    Update project

---
+++ Api/Data/ScuolaDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Api.Data
+{
+    public class ScuolaDbContext : DbContext
+    {
+        public ScuolaDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Aula> Aule { get; set; }
+        DbSet<Classe> Classi { get; set; }
+        DbSet<Corso> Corsi { get; set; }
+        DbSet<Docente> Docenti { get; set; }
+        DbSet<Iscrizione> Iscrizioni { get; set; }
+        DbSet<Lezione> Lezioni { get; set; }
+        DbSet<Presenza> Presenze { get; set; }
+        DbSet<Studente> Studenti { get; set; }
+        DbSet<Valutazione> Valutazioni { get; set; }
+        DbSet<Voto> Voti { get; set; }
+    }
+}
