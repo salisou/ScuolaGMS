df29fb1350e2c690d35ee68c09e61e948a12d3a7
Moussa <salisoumoussa1@gmail.com>
2025-11-28 10:18:46 +0100

    Updade Appsettings and program 28_11_25

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Api.Data;
+using Api.Services;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +12,27 @@ var strConnect = builder.Configuration.GetConnectionString("strCon") ??
 
 builder.Services.AddDbContext<ScuolaDbContext>(options => options.UseSqlServer(strConnect));
 
+#region Generic Repository and Services
+//builder.Services.AddScoped(typeof(IGenericRepository<>, typeof(Repository<>));
+builder.Services.AddScoped<AulaService>();
+//builder.Services.AddScoped<>();
+//builder.Services.AddScoped<>();
+//builder.Services.AddScoped<>();
+//builder.Services.AddScoped<>();
+//builder.Services.AddScoped<>();
+//builder.Services.AddScoped<>();
+//builder.Services.AddScoped<>();
+//builder.Services.AddScoped<>();
+//builder.Services.AddScoped<>();
+#endregion
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Log del api salvato in .log-.txt
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
 
 var app = builder.Build();
 
