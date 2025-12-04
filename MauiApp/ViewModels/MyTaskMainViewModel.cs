635297542a15ff4c24f5a7e10dd3aaa3ae0d5788
Moussa <salisoumoussa1@gmail.com>
2025-12-04 13:02:51 +0100

    Update MyTaskMain

---
+++ MauiApp/ViewModels/MyTaskMainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MauiApp.Models;
+using System.Collections.ObjectModel;
+
+namespace MauiApp.ViewModels
+{
+    public class MyTaskMainViewModel
+    {
+        public ObservableCollection<Category> Categories { get; set; }
+
+        public MyTaskMainViewModel()
+        {
+            FillData();
+        }
+
+        private void FillData()
+        {
+            Categories = new ObservableCollection<Category>
+            {
+                new() {
+                        Id = 1,
+                        CategoryName = ".NET MAUI Course",
+                        Color = "#CF14DF"
+                },
+                new() {
+                        Id = 2,
+                        CategoryName = "Tutorials",
+                        Color = "#df6f14"
+                },
+                new() {
+                        Id = 3,
+                        CategoryName = "Shopping",
+                        Color = "#14df80"
+                }
+            };
+        }
+    }
+}
